c65daf8b8095ab5a2e93dc803a4a28debde49da5
qicaisheng <qicaisheng@qq.com>
2020-06-11 14:33:19 +0800

    get current workspace path from txt

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 declare currentWorkspacePath
+DATA_DIRECTORY='./data'
 
 function prepare_workspace {
     sh prepare_workspace.sh
 }
 
+function get_current_workspace_path {
+    currentWorkspacePath=$(cat $DATA_DIRECTORY/currentWorkspacePath.txt)
+    echo "current workspace path is: $currentWorkspacePath"
+}
+
 function main {
     prepare_workspace
+    get_current_workspace_path
 }
 
 main
