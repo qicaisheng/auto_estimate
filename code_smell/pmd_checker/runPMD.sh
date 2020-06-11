2a501d69591a93319e7dedb5291873a2e879aa14
qicaisheng <qicaisheng@qq.com>
2020-06-11 15:17:56 +0800

    rename

--- code_smell/pmd_checker/runPMD.sh
+++ code_smell/pmd_checker/runPMD.sh
@@ -5,17 +5,17 @@ PMD_RULESETS_NAME="pmd-rulesets.xml"
 DATA_DIRECTORY='../../data'
 PMD_RESULT_REPORT_JSON_NAME="pmd-result-report.json"
 PMD_RESULT_REPORT_HTML_NAME="pmd-result-report.html"
-declare codeRepositoryFolderPath
+declare currentWorkspacePath
 
 function readCurrentWorkspacePath {
-    codeRepositoryFolderPath=$(cat $DATA_DIRECTORY/currentWorkspacePath.txt)
+    currentWorkspacePath=$(cat $DATA_DIRECTORY/currentWorkspacePath.txt)
 }
 
 function executePMD {
-    ./${PMD_BIN_FOLDER_NAME}/bin/run.sh pmd -d ${codeRepositoryFolderPath} -f json -R ./${PMD_RULESETS_NAME} -r ${codeRepositoryFolderPath}/${PMD_RESULT_REPORT_JSON_NAME}
-    ./${PMD_BIN_FOLDER_NAME}/bin/run.sh pmd -d ${codeRepositoryFolderPath} -f html -R ./${PMD_RULESETS_NAME} -r ${codeRepositoryFolderPath}/${PMD_RESULT_REPORT_HTML_NAME}
+    ./${PMD_BIN_FOLDER_NAME}/bin/run.sh pmd -d ${currentWorkspacePath} -f json -R ./${PMD_RULESETS_NAME} -r ${currentWorkspacePath}/${PMD_RESULT_REPORT_JSON_NAME}
+    ./${PMD_BIN_FOLDER_NAME}/bin/run.sh pmd -d ${currentWorkspacePath} -f html -R ./${PMD_RULESETS_NAME} -r ${currentWorkspacePath}/${PMD_RESULT_REPORT_HTML_NAME}
 
-	echo "PMD report is: ${codeRepositoryFolderPath}/${PMD_RESULT_REPORT_HTML_NAME}"
+	echo "PMD report is: ${currentWorkspacePath}/${PMD_RESULT_REPORT_HTML_NAME}"
 }
 
 function main {
