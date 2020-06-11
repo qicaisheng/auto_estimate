35e6895188f850daf901ffd8aa277b5e168bf4ba
qicaisheng <qicaisheng@qq.com>
2020-06-11 15:36:25 +0800

    parse pmd report from workspace

--- code_smell/score_calculator/src/score_calculator.py
+++ code_smell/score_calculator/src/score_calculator.py
@@ -1,18 +1,25 @@
 import json
 
-PMD_REPORT = '../../pmd_checker/pmd-result-report.json'
+PMD_REPORT_NAME = 'pmd-result-report.json'
+CURRENT_WORKSPACE_DATA = '../../../data/currentWorkspacePath.txt'
 FULL_SCORE = 20
 PRIORITY_DEDUCT_MARKS = {1: 3, 2: 2, 3: 0.5, 4: 0.2, 5: 0.1}
 
 
-def get_report():
-    with open(PMD_REPORT) as json_file:
+def get_pmd_report():
+    pmd_report_path = get_current_workspace_path().strip() + "/" + PMD_REPORT_NAME
+    with open(pmd_report_path) as json_file:
         return json.load(json_file)
+
+
+def get_current_workspace_path():
+    with open(CURRENT_WORKSPACE_DATA) as text_file:
+        return text_file.read()
 
 
 def calculate_score():
     score = FULL_SCORE
-    data = get_report()
+    data = get_pmd_report()
     for checked_file in data['files']:
         for violation in checked_file['violations']:
             deduct_marks = PRIORITY_DEDUCT_MARKS[violation['priority']]
