2016807ccd1f1292d8d2b6583e64bda779e2b7ab
qicaisheng <qicaisheng@qq.com>
2020-06-12 17:11:24 +0800

    save test coverage score to txt

--- test_coverage/score_calculator/src/score_calculator.py
+++ test_coverage/score_calculator/src/score_calculator.py
@@ -3,16 +3,17 @@ import xml.etree.ElementTree as ET
 JACOCO_REPORT_NAME = 'jacoco.xml'
 CURRENT_WORKSPACE_DATA = '../../../data/currentWorkspacePath.txt'
 FULL_SCORE = 10
+SCORE_REPORT_NAME = 'score.txt'
 
 
 def get_jacoco_report():
-    jacoco_report_path = get_current_workspace_path().strip() + "/jacoco/" + JACOCO_REPORT_NAME
+    jacoco_report_path = get_current_workspace_path() + "/jacoco/" + JACOCO_REPORT_NAME
     return ET.parse(jacoco_report_path).getroot()
 
 
 def get_current_workspace_path():
     with open(CURRENT_WORKSPACE_DATA) as text_file:
-        return text_file.read()
+        return text_file.read().strip()
 
 
 def calculate_score():
@@ -28,7 +29,15 @@ def calculate_score():
     return (instruction_percentage + branch_percentage) / 2 * FULL_SCORE
 
 
+def save_score():
+    score_path = get_current_workspace_path() + "/" + SCORE_REPORT_NAME
+    f = open(score_path, "a")
+    f.write("Test Coverage Score: " + str(calculate_score()) + "\n")
+    f.close()
+
+
 def main():
+    save_score()
     print('test coverage score is: {}'.format(calculate_score()))
 
 
